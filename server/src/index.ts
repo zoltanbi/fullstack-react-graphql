eefbc3fd089ce811c9e110998da2b257dc3a9686
Zoltanbi <zoltanbiro001@gmail.com>
2021-10-03 12:34:33 -0700

    Added Post entity, production constant and preliminary database config

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,26 @@
-console.log("hey there");
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import 'dotenv/config';
+
+const main = async () => {
+    const orm = await MikroORM.init({
+        entities: [Post],
+        dbName: 'forum',
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        type: 'postgresql',
+        debug: !__prod__,
+    });
+
+    const post = orm.em.create(Post, {title: 'my first post'})
+    await orm.em.persistAndFlush(post);
+    console.log("----------sql 2-----------")
+    await orm.em.nativeInsert(Post, { title: "my first post 2"})
+};
+
+main().catch((err) => {
+    console.error(err);
+})
+
+
