d0182f9febd4e7b8cf4b0fca03aa3f1538589c3a
Zoltanbi <zoltanbiro001@gmail.com>
2021-10-04 15:40:36 -0700

    Installed express, graphQL and apollo-server-express. Added basic express middleware to test

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,15 +3,21 @@ import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import 'dotenv/config';
 import mikroConfig from "./mikro-orm.config";
+import express from 'express';
 
 const main = async () => {
     const orm = await MikroORM.init(mikroConfig);
     await orm.getMigrator().up();
-    // const post = orm.em.create(Post, {title: 'my first post'})
-    // await orm.em.persistAndFlush(post);
 
-    // const posts = await orm.em.find(Post, {});
-    // console.log(posts);
+    const app = express();
+
+    app.get('/', (_, res) => {
+        res.send("hello");
+    });
+
+    app.listen(3000, () => {
+        console.log('server started on localhost:3000')
+    });
 };
 
 main().catch((err) => {
