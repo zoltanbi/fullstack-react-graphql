90d5f80689d0f5325d46fcf5c184ea94e04997ce
Zoltanbi <zoltanbiro001@gmail.com>
2021-10-04 17:07:53 -0700

    Added a posts resolver which return all Posts from the database.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,4 @@
+import 'reflect-metadata';
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
@@ -7,6 +8,7 @@ import express from 'express';
 import { ApolloServer } from 'apollo-server-express'
 import { buildSchema } from 'type-graphql';
 import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
     const orm = await MikroORM.init(mikroConfig);
@@ -16,9 +18,10 @@ const main = async () => {
 
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
-            resolvers: [HelloResolver],
+            resolvers: [HelloResolver, PostResolver],
             validate: false
         }),
+        context: () => ({ em: orm.em })
     });
 
     await apolloServer.start()
