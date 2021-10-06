74cfc65ee8c18d923bf4241ce4925edf03dcf062
Zoltanbi <zoltanbiro001@gmail.com>
2021-10-05 22:52:00 -0700

    Created the user migration

--- server/src/mikro-orm.config.ts
+++ server/src/mikro-orm.config.ts
@@ -1,5 +1,6 @@
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
+import { User } from "./entities/User";
 import { MikroORM } from '@mikro-orm/core';
 import 'dotenv/config';
 import path from 'path';
@@ -9,7 +10,7 @@ export default {
         path: path.join(__dirname, './migrations'),
         pattern: /^[\w-]+\d+\.[tj]s$/,
     },
-    entities: [Post],
+    entities: [Post, User],
     dbName: 'forum',
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
