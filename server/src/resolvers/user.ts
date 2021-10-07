4d6379291d7b3cf2c04350bebf393e975b6bb9af
Zoltanbi <zoltanbiro001@gmail.com>
2021-10-06 20:38:42 -0700

    Added username and password requirements and no duplicate username verification

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -59,14 +59,14 @@ export class UserResolver {
     }
 
     if (options.password.length <= 3) {
-        return {
-            errors: [
-              {
-                field: "password",
-                message: "length must be greater than 3",
-              },
-            ],
-          };
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "length must be greater than 3",
+          },
+        ],
+      };
     }
 
     const hashedPassword = await argon2.hash(options.password);
@@ -74,9 +74,27 @@ export class UserResolver {
       username: options.username,
       password: hashedPassword,
     });
-    await em.persistAndFlush(user);
+    try {
+      await em.persistAndFlush(user);
+    } catch (err) {
+      //duplicate username error
+      if (err.code === "23505") {
+        //|| err.detail.includes("already exists")) {}
+        return {
+          errors: [
+            {
+              field: "username",
+              message: "username already taken",
+            },
+          ],
+        };
+      }
+
+      console.log("message: ", err.message);
+    }
+
     return {
-        user
+      user,
     };
   }
 
