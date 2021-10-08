7c6c08e59d8a04966ed453227a8ca1bf35095994
Zoltanbi <zoltanbiro001@gmail.com>
2021-10-08 14:39:03 -0700

    Created a register page with a test Formik form. Created a Wrapper component for styling

---
+++ client/src/pages/register.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Form, Formik } from "formik";
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+} from "@chakra-ui/form-control";
+import { Input } from "@chakra-ui/input";
+import { Wrapper } from "../components/Wrapper";
+
+interface registerProps {}
+
+export const Register: React.FC<registerProps> = ({}) => {
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ values, handleChange }) => (
+          <Form>
+            <FormControl>
+              <FormLabel htmlFor="username">Username</FormLabel>
+              <Input
+                value={values.username}
+                onChange={handleChange}
+                id="username"
+                placeholder="username"
+              />
+              {/* <FormErrorMessage>{form.errors.name}</FormErrorMessage> */}
+            </FormControl>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+export default Register;
