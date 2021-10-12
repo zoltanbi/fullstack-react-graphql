ac562b3a2ca1390a1349ec50592318e69eb16341
Zoltanbi <zoltanbiro001@gmail.com>
2021-10-12 15:49:31 -0700

    created useMutation hook for registering a user

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -9,16 +9,37 @@ import { Input } from "@chakra-ui/input";
 import { Wrapper } from "../components/Wrapper";
 import { InputField } from "../components/InputField";
 import { Box, Button } from "@chakra-ui/react";
+import { useMutation } from "urql";
 
 interface registerProps {}
 
+const REGISTER_MUTATION = `
+mutation Register($username: String!, $password: String!) {
+  register(options: { username: $username, password: $password }) {
+    errors {
+      field
+      message
+    }
+    user {
+      id
+      username
+    }
+  }
+}
+`
+
 export const Register: React.FC<registerProps> = ({}) => {
+
+  const [,register] = useMutation(REGISTER_MUTATION);
+
+
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
         onSubmit={(values) => {
           console.log(values);
+          register(values)
         }}
       >
         {({ isSubmitting }) => (
