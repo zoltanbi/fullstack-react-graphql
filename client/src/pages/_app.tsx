81aaa715e49bccddaafe5a7becca2d38367ded26
Zoltanbi <zoltanbiro001@gmail.com>
2021-10-12 15:37:14 -0700

    Installed urql and wrapped _app with the urql provider pointing to the server

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,18 +1,24 @@
 import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
-
+import { createClient, Provider } from 'urql';
 import theme from '../theme'
+
+const client = createClient({
+  url: 'http://localhost:4000/graphql',
+});
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <ColorModeProvider
-        options={{
-          useSystemColorMode: true,
-        }}
-      >
-        <Component {...pageProps} />
-      </ColorModeProvider>
-    </ChakraProvider>
+    <Provider value={client}>
+      <ChakraProvider resetCSS theme={theme}>
+        <ColorModeProvider
+          options={{
+            useSystemColorMode: true,
+          }}
+        >
+          <Component {...pageProps} />
+        </ColorModeProvider>
+      </ChakraProvider>
+    </Provider>
   )
 }
 
